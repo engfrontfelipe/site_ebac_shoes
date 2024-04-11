fcbaba2b14fcf453adce4c13e6355f7ec4514846
Felipe Maciel <engfrontfelipe@gmail.com>
2024-04-10 21:22:49 -0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+$(`#btn1`).click(function(){
+     $(`html, body`).animate({scrollTop : $(`#about`). offset().top}, 800);
+})
+   
+
+$(`#btn2`).click(function(){
+    $(`html, body`).animate({scrollTop : $(`#buy`). offset().top}, 800)
+
+})
